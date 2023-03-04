f406a6f1fce89fe25491128210461cb581be276e
Gaurang Ruparelia <gr2159@nyu.edu>
2023-03-04 18:47:31 -0500

    complete everything except q5.4

--- Homeworks/helper_functions.py
+++ Homeworks/helper_functions.py
@@ -9,6 +9,19 @@ get_linear_angular_velocity_from_twist = lambda twist: (twist[:3][:3], twist[3:]
 
 create_twist_from_linear_angular_velocity = lambda linear_velocity, angular_velocity: np.vstack((linear_velocity, angular_velocity))
 
+def get_twist_skew(twist):
+    w, v = get_linear_angular_velocity_from_twist(twist)
+    w_skew = get_skew(w)
+    twist_skew = np.zeros((4, 4))
+
+    twist_skew[0:3, 0:3] = w_skew
+    
+    # ! twist_skew[:3, -1] returns a view of the last column of twist_skew but it is a row vector, v is a column vector
+    # ! so we need to transpose v to make it a row vector and set it by reference
+    twist_skew[:3, -1] = v.T
+
+    return twist_skew
+
 # Define the rotation matrices for each axis
 
 def get_x_rotation_matrix(theta, is_hybrid = False):
