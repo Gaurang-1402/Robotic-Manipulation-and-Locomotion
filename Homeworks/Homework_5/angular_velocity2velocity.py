c3412f66cd18c032724b63bed8325c928a6c13e8
Gaurang Ruparelia <gr2159@nyu.edu>
2023-03-06 22:18:30 -0500

    make the prints in Yinlongs files easier

--- Homeworks/Homework_5/angular_velocity2velocity.py
+++ Homeworks/Homework_5/angular_velocity2velocity.py
@@ -1,9 +1,9 @@
 #cowriter: Github Copilot
 import numpy as np 
-R01 = np.array([[-0.119365806805, -0.271075612465, -0.955128167574], [0.174448984216, -0.952766346110, 0.248603784415], [-0.977404397408, -0.136946347327, 0.161016588840]]) 
-w0 = np.array([[0.420000000000], [-0.820000000000], [0.810000000000]]) 
-t = 7.030000000000 
-p1 = np.array([[1.060000000000], [2.330000000000], [-1.500000000000]])
+R01 = np.array([[-0.882524056769, 0.427920181548, 0.195027196689], [-0.101145567447, 0.232288125516, -0.967373661483], [-0.459261214789, -0.873456664620, -0.161717629285]])
+w0 = np.array([[3.010000000000], [1.710000000000], [2.030000000000]]) 
+t = 2.350000000000
+p1 = np.array([[-2.930000000000], [-2.840000000000], [2.930000000000]])
 
 
 lalaland = input("input v1 if you want v1p1, input v0 if you want v0p0:")
@@ -21,8 +21,8 @@ if lalaland == "v1":
     v1 = np.array2string(v1, separator=',')
     #change p1 to be comma separated
     p1 = np.array2string(p1, separator=',')
-    print (v1)
-    print (p1)
+    print ("v1", v1)
+    print ("p1", p1)
 
 
 if lalaland == "v0":
@@ -49,7 +49,7 @@ if lalaland == "v0":
     v0 = np.array2string(v0, separator=',')
     #change p2 to be comma separated
     p2 = np.array2string(p2, separator=',')
-    print (v0)
-    print (p2)
+    print ("v0", v0)
+    print ("p2", p2)
 
 
